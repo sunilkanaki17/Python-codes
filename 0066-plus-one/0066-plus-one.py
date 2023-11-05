e58cc63c91f446da980eb78ab39aa563d883cfa1
Sunil Kanaki <142711444+sunilkanaki17@users.noreply.github.com>
2023-11-05 07:25:30 +0530

    Time: 36 ms (79.33%), Space: 16.2 MB (36.53%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,18 +1,12 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        lst=[1]
         result=[]
         add=0
-        mult=1
-        for i in range(len(digits)-1):
-            lst.append(lst[i]*10)
-        lst.reverse()
         for i in range(len(digits)):
-            mult = digits[i] * lst[i]
-            add = add+mult
+            add +=(digits[i] * (pow(10,len(digits)-i-1)))
         add+=1
-        op=str(add)
-        for i in range(len(op)):
-            result.append(int(op[i])) 
+        add=str(add)
+        for i in add:
+            result.append(int(i)) 
         return result
         
